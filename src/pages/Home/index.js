1afc3b63e46aa985f06635cadee448843090e989
N-LUSANO <n.lusano@gmail.com>
2020-07-14 21:21:56 +0200

    Added trip form

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,11 +1,11 @@
 import React from "react";
-
+import NewTripForm from "../../components/NewTripForm";
 
 export default function Home() {
-
-    return (
-        <div>
-            Home
-        </div>
-    )
+  return (
+    <div>
+      Home
+      <NewTripForm />
+    </div>
+  );
 }
