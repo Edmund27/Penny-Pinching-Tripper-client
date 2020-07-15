f0b26fa1fade47abf748c2f829416c6021e19b09
edmund27 <epafko@gmail.com>
2020-07-15 10:26:04 +0200

    geolocation

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,11 +1,24 @@
 import React from "react";
 
-
 export default function Home() {
+
+
+    function getLocation() {
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition(showPosition);
+        } else {
+            console.log('geolocation not supported');
+        }
+    }
+
+    function showPosition(position) {
+        console.log("THIS IS YOU POSITION", position)
+    }
 
     return (
         <div>
             Home
+            <button onClick={getLocation}> Get Location</button>
         </div>
     )
 }
