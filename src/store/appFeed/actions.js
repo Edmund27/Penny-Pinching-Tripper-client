240f283c92c096e683e5eef47d5df7d7535ba203
edmund27 <epafko@gmail.com>
2020-08-20 00:52:26 +0200

    fetching and displaying restaurants/hotels on map based on the city, price and type

--- src/store/appFeed/actions.js
+++ src/store/appFeed/actions.js
@@ -1,4 +1,5 @@
-
+import { apiUrl } from "../../config/constants";
+import axios from "axios";
 
 export const storePosition = (data) => {
     return {
@@ -7,3 +8,41 @@ export const storePosition = (data) => {
     }
 }
 
+export const setCurrentTrip = (city, country) => {
+    console.log("ACTIONS.js", city, country)
+    return {
+        type: "SET_CURRENT_TRIP",
+        payload: [city, country]
+    }
+}
+
+export const storeBusinesses = (businesses) => {
+    //console.log("ACTIONS.js", businesses)
+    return {
+        type: "STORE_BUSINESSES",
+        payload: businesses
+    }
+}
+
+export const fetchBusinesses = (city, businessType, priceCategory) => {
+    return async (dispatch, getState) => {
+        console.log("LAST BEFORE DISPTCH", businessType, priceCategory)
+
+        try {
+            const response = await axios.post(`${apiUrl}/yelp/${city}`, {
+                businessType,
+                priceCategory
+
+            })
+            console.log(response.data)
+            dispatch(storeBusinesses(response.data))
+
+        } catch (error) {
+            if (error.response) {
+                console.log(error.response.message);
+            } else {
+                console.log(error);
+            }
+        }
+    };
+};
--- src/store/appFeed/selectors.js
+++ src/store/appFeed/selectors.js
@@ -1,3 +1,15 @@
 export const selectCurrentPosition = (state) => {
     return state.appFeed.currentPosition
 }
+
+export const selectCurrentTrip = (state) => {
+    return state.appFeed.currentTrip
+}
+
+export const selectTripCoordinates = (state) => {
+    return state.appFeed.currentTrip.businesses.region.center
+}
+
+export const selectBusinesses = (state) => {
+    return state.appFeed.currentTrip.businesses
+}
